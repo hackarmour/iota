818a30b45bcd1468f44ef16bde93923c4fc9befd
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-08 13:13:18 +0530

    added a basic form

--- client/src/home/create.jsx
+++ client/src/home/create.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {useFormik} from 'formik';
 
 function CreateEntity() {
-    return (<div>123</div>)
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            description: ''
+        },
+        onSubmit: values => {
+            console.log(values)
+        }
+    })
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+            />
+
+            <label htmlFor="description">Description</label>
+            <input
+                id="description"
+                name="description"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.description}
+            />
+        <button type="submit">Submit</button>
+       </form>
+    )
 }
 
 export default CreateEntity;
--- client/src/home/nav.jsx
+++ client/src/home/nav.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Logo, NavWrapper } from '../components/nav';
+import { Link } from 'react-router-dom';
 
 
 class Nav extends React.Component {
     render() {
         return (
             <NavWrapper>
-                <Logo>IOTA</Logo>
+                <Link to="/" style={{textDecoration: 'none'}}>
+                    <Logo>IOTA</Logo>
+                </Link>
             </NavWrapper>
         )
     }
