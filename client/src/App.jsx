df390de545f735796eb23dc2017e37e8bf473419
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-18 14:36:24 +0530

    fixed some routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import './fonts.css';
 import './App.sass';
 
 import Home from './home/home';
-import CreateEntity from './home/create';
+import CreateProject from './home/create';
+import CreateEntity from './main/createEntity';
 import Main from './main/main';
 
 class App extends React.Component {
@@ -15,10 +16,10 @@ class App extends React.Component {
       <div>
           <Nav />
           <Route path="/" exact component={Home} />
-          <Route path="/create" component={CreateEntity} />
+          <Route path="/create" component={CreateProject} />
 
           <Route path="/project/:id" component={Main} exact />
-          <Route path="/project/:id/new" />
+          <Route path="/project/:id/new" component={CreateEntity} />
       </div>
     )
   }
