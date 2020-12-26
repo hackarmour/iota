be130f0229f205b65b718196a2ae8a7540e2a91d
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-26 16:05:04 +0530

    services refractor

--- client/src/services/entity.jsx
+++ client/src/services/entity.jsx
@@ -2,15 +2,11 @@ import axios from "axios"
 import { ENDPOINT } from '../url';
 
 export const EntityService = {
-    getEntities: async function() {
-        return await axios.get(ENDPOINT)
-    },
-
     postEntry: async function(data) {
-        return await axios.post(ENDPOINT, {data})
+        return await axios.post(ENDPOINT+'/entity', {data})
     },
 
     deleteEntry: async function(id) {
-        return axios.delete(ENDPOINT+id)
+        return axios.delete(ENDPOINT+'/entity/'+id)
     }
 } 
--- client/src/services/project.jsx
+++ client/src/services/project.jsx
@@ -2,11 +2,15 @@ import axios from 'axios';
 import { ENDPOINT } from '../url';
 
 export const ProjectService = {
-    getProject: async function(id) {
+    getOneProject: async function(id) {
         return await axios.get(ENDPOINT+'projects/'+id)
     },
 
+    getProjects: async function() {
+        return await axios.get(ENDPOINT+'projects')
+    },
+
     createProject: async function(data) {
-        return await axios.post(ENDPOINT+'projects/', {data})
+        return await axios.post(ENDPOINT+'projects', {data})
     }
 }
